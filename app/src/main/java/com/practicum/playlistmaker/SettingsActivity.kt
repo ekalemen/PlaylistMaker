270959347055bcdecce6d4047a7e1ba1f88c0fa8
Eugeny Kalemenev <e.kalemenev@yadro.com>
2024-11-04 21:34:24 +0300

    Sprint #8: Homework
    This adds MainActivity,Settings Activity and simple navigation between them.
    Also adds templates for Media and Search activities.
    
    Signed-off-by: Eugeny Kalemenev <e.kalemenev@yadro.com>

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val buttonSettingsBack = findViewById<ImageView>(R.id.settings_button_back)
+
+        buttonSettingsBack.setOnClickListener {
+            val setBackToMainIntent = Intent(this, MainActivity::class.java)
+            startActivity(setBackToMainIntent)
+        }
+    }
+}
