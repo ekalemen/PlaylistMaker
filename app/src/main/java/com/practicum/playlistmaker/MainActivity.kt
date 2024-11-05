bee2d00d70acbad2ceef36b57ddcfa38c13a76d5
Eugeny Kalemenev <e.kalemenev@yadro.com>
2024-11-05 23:48:31 +0300

    main: fix drawable colors and activity stack overflow issue
    This adds to vector drawables common colors from colors.xml file.
    And fixes activity stack overflow by finishing current activity before
    start a new one.
    
    Signed-off-by: Eugeny Kalemenev <e.kalemenev@yadro.com>

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -16,24 +16,6 @@ class MainActivity : AppCompatActivity() {
         val buttonMainMedia = findViewById<Button>(R.id.main_butt_media)
         val buttonMainSettings = findViewById<Button>(R.id.main_butt_settings)
 
-        /*val buttonOnClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                when(v?.id) {
-                    R.id.main_butt_search -> {
-                        Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.main_butt_media -> {
-                        Toast.makeText(this@MainActivity, "Нажали на кнопку Медиа", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.main_butt_settings -> {
-                        Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-            }
-        }*/
-
-
         buttonMainSearch.setOnClickListener {
             val searchIntent = Intent(this, SearchActivity::class.java)
             startActivity(searchIntent)
