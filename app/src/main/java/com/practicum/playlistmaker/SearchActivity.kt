7c304a48d35cedce70524c64e52837d602abaada
Eugeny Kalemenev <e.kalemenev@yadro.com>
2024-11-21 22:52:22 +0300

    Sprint #9: Homework
    This adds fonts and styles(day/night) for all activities layouts and adds new
    Search Activity.
    
    Signed-off-by: Eugeny Kalemenev <e.kalemenev@yadro.com>

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,59 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+
+        @SuppressLint("ClickableViewAccessibility")
+        fun EditText.onRightDrawableClicked(onClicked: (view: EditText) -> Unit) {
+            this.setOnTouchListener { v, event ->
+                var hasConsumed = false
+                if (v is EditText) {
+                    if (event.x >= v.width - v.totalPaddingRight) {
+                        if (event.action == MotionEvent.ACTION_UP) {
+                            onClicked(this)
+                        }
+                        hasConsumed = true
+                    }
+                }
+                hasConsumed
+            }
+        }
+
+        searchEditText.onRightDrawableClicked {
+            it.text.clear()
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.search_edit_view_icon, 0, 0, 0);
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(s.isNullOrEmpty()) {
+                    searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.search_edit_view_icon, 0, 0, 0);
+                } else {
+                    searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.search_edit_view_icon, 0, R.drawable.edit_clear, 0);
+                }
+            }
+        }
+
+        searchEditText.addTextChangedListener(searchTextWatcher)
     }
 }
